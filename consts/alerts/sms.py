6508dae6a7f10db8fa116c04a99aad1666b3afdc
NAVNAV221 <navve2008@gmail.com>
2023-03-24 14:31:02 +0300

    Initial commit

---
+++ consts/alerts/sms.py
@@ -0,0 +1,23 @@
+class System:
+    SENDER = 'whatsapp:+14155238886'
+    RECEIVER = 'whatsapp:+{phone_number}'
+
+class Twilio:
+    ACCOUNT_SID = 'ACb7d19b3ab3c7721b90afa5aae45e9cda'
+    AUTH_TOKEN = '422e42e095b93a91767b6a6fb1b9db27'
+
+class Templates:
+    MATCHED_TICKERS_SMS = """
+    Hey {username} 👋
+    ⚠️ IStoBot ALERT ⚠️
+    
+    -------------------
+    🚨 Alert from the following filters:
+    -------------------
+    {filters}
+    
+    -------------------
+    Matched tickers📈
+    -------------------
+    {matched_wanted_tickers}
+    """
---
+++ consts/general/project.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+class Consts:
+    PROJECT_ROOT_PATH = Path(r'c:\Users\navnav221\PycharmProjects\IStoBot')
+    PROJECT_JSON_INPUT_PATH = PROJECT_ROOT_PATH.joinpath('Input')
