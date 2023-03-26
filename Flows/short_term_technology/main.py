9abd9bd0c22987b4d9b3336599c133712f4e296b
NAVNAV221 <navve2008@gmail.com>
2023-03-26 23:13:50 +0300

    Change flow location

--- Flows/short_term_technology/main.py
+++ Flows/short_term_technology/main.py
@@ -1,10 +1,26 @@
+from time import sleep
 from typing import List
 
 from Alerts.sms.sms import Sms
-from filters.screener.platforms.finviz_platform import FinvizScreener
 from consts.alerts.sms import System, Templates
+from filters.screener.platforms.finviz_platform import FinvizScreener
 
 
+def cron_job(seconds=5):
+    def job(func):
+        def wrapper(*args, **kwargs):
+            while True:
+                print('Start execute function ...')
+                func(*args, **kwargs)
+                print(f'Finish execution ...')
+                sleep(seconds)
+
+        return wrapper
+
+    return job
+
+
+@cron_job(seconds=43200)
 def main():
     scanner = FinvizScreener()
     alert = Sms()
@@ -18,19 +34,6 @@ def main():
     alert.send_sms(body=alert_data, sender=System.SENDER,
                    receiver=System.RECEIVER.format(phone_number=TEST_PHONE_NUMBER))
     print(f'Message sent')
-
-    # for ticker in wanted_tickers:
-    #     if ticker in matched_scanner_tickers:
-    #         matched_wanted_tickers.append(ticker)
-    # if matched_wanted_tickers:
-    #     formatted_matched_wanted_tickers = " - \n".join(matched_wanted_tickers)
-    #     alert_data = Templates.MATCHED_TICKERS_SMS.format(username='Nave', filters=scanner.format_filters(),
-    #                                                       matched_wanted_tickers=formatted_matched_wanted_tickers)
-    #
-    #     alert.send_sms(body=alert_data, sender=System.SENDER,
-    #                    receiver=System.RECEIVER.format(phone_number=TEST_PHONE_NUMBER))
-    #     print(f'Message sent')
-
 
 
 if __name__ == '__main__':
