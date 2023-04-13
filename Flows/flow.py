68c6a80a9185a2f270fa746b066181d9372378dc
NAVNAV221 <navve2008@gmail.com>
2023-04-14 00:17:56 +0300

    Add flow's class

--- Flows/flow.py
+++ Flows/flow.py
@@ -1,15 +1,16 @@
+import logging
 import uuid
-from typing import List
 
-from Logics.logic import Logic
 from consts.flows.flow import Types
+from utils.project_logger import get_logger
 
 
 class Flow(object):
-    def __init__(self, name: str, logics: Types.FLOW_LOGICS):
+    def __init__(self, name: str, logics: Types.FLOW_LOGICS=None):
+        self.logger = get_logger()
         self._id = uuid.uuid4()
         self.name = name
         self.logics: Types.FLOW_LOGICS = logics
 
     def run(self):
-        pass
+        raise NotImplementedError("run function not implemented")
--- Flows/short_term_technology/main.py
+++ Flows/short_term_technology/main.py
@@ -1,54 +1,27 @@
-from time import sleep
 from typing import List
 
 from Alerts.sms.sms import Sms
-from consts.alerts.sms import System, Templates
-from Logics.bear_market.is_bear_market import BearMarketLogic
 from Flows.flow import Flow
-
+from Logics.bear_market.is_bear_market import BearMarketLogic
+from consts.alerts.sms import System, Templates
 from filters.screener.platforms.finviz_platform import FinvizScreener
-
-
-def cron_job(seconds=5):
-    def job(func):
-        def wrapper(*args, **kwargs):
-            while True:
-                print('Start execute function ...')
-                func(*args, **kwargs)
-                print(f'Finish execution ...')
-                sleep(seconds)
-
-        return wrapper
-
-    return job
-
-
-@cron_job(seconds=43200)
-def main():
-    scanner = FinvizScreener()
-    alert = Sms()
-    TEST_PHONE_NUMBER = '972526368078'
-
-    # if is_bear_market.BearMarketLogic.run(ticker='SPY'):
-    #     pass
-    bear_market_logic = BearMarketLogic()
-    is_bear_market: bool = bear_market_logic.run(ticker='SPY')
-
-    if is_bear_market:
-        matched_scanner_tickers: List[str] = scanner.get_matched_tickers()
-        print(f'matched_scanner_tickers: {matched_scanner_tickers}')
-        alert_data = Templates.MATCHED_TICKERS_SMS.format(username='Nave', filters=scanner.format_filters(),
-                                                          matched_wanted_tickers=matched_scanner_tickers)
-
-        alert.send_sms(body=alert_data, sender=System.SENDER,
-                       receiver=System.RECEIVER.format(phone_number=TEST_PHONE_NUMBER))
-        print(f'Message sent')
 
 
 class ShortTermTechnologyFlow(Flow):
     def run(self):
         bear_market_logic = BearMarketLogic()
+        scanner = FinvizScreener()
+        alert = Sms()
+        TEST_PHONE_NUMBER = '972526368078'
 
+        is_bear_market: bool = bear_market_logic.run(ticker='SPY')
 
-if __name__ == '__main__':
-    main()
+        if is_bear_market:
+            matched_scanner_tickers: List[str] = scanner.get_matched_tickers()
+            self.logger(f'matched_scanner_tickers: {matched_scanner_tickers}')
+            alert_data = Templates.MATCHED_TICKERS_SMS.format(username='Nave', filters=scanner.format_filters(),
+                                                              matched_wanted_tickers=matched_scanner_tickers)
+
+            alert.send_sms(body=alert_data, sender=System.SENDER,
+                           receiver=System.RECEIVER.format(phone_number=TEST_PHONE_NUMBER))
+            self.logger(f'Message sent')
