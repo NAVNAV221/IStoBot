93267bf1c119b51b5b5cd2bf26e1148e0a1a4086
NAVNAV221 <navve2008@gmail.com>
2023-04-13 23:18:24 +0300

    Remove unnecessary tests

--- consts/general/project.py
+++ consts/general/project.py
@@ -5,7 +5,3 @@ class Consts:
     PROJECT_ROOT_PATH = Path(r'c:\Users\navnav221\PycharmProjects\IStoBot')
     PROJECT_FILTERS_RELATIVE_PATH = Path(r'filters')
     PROJECT_JSON_INPUT_RELATIVE_PATH = PROJECT_ROOT_PATH.joinpath(PROJECT_FILTERS_RELATIVE_PATH.joinpath('Input'))
-
-
-if __name__ == '__main__':
-    print(Consts.PROJECT_JSON_INPUT_RELATIVE_PATH)
