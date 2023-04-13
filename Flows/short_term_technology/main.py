936c7fabc3d26c62402cf77c3ab16877d52a8ead
NAVNAV221 <navve2008@gmail.com>
2023-04-13 21:28:29 +0300

    Add symbol highest price checking

--- Flows/short_term_technology/main.py
+++ Flows/short_term_technology/main.py
@@ -3,6 +3,9 @@ from typing import List
 
 from Alerts.sms.sms import Sms
 from consts.alerts.sms import System, Templates
+from Logics.bear_market.is_bear_market import BearMarketLogic
+from Flows.flow import Flow
+
 from filters.screener.platforms.finviz_platform import FinvizScreener
 
 
@@ -26,14 +29,25 @@ def main():
     alert = Sms()
     TEST_PHONE_NUMBER = '972526368078'
 
-    matched_scanner_tickers: List[str] = scanner.get_matched_tickers()
-    print(f'matched_scanner_tickers: {matched_scanner_tickers}')
-    alert_data = Templates.MATCHED_TICKERS_SMS.format(username='Nave', filters=scanner.format_filters(),
-                                                      matched_wanted_tickers=matched_scanner_tickers)
+    # if is_bear_market.BearMarketLogic.run(ticker='SPY'):
+    #     pass
+    bear_market_logic = BearMarketLogic()
+    is_bear_market: bool = bear_market_logic.run(ticker='SPY')
 
-    alert.send_sms(body=alert_data, sender=System.SENDER,
-                   receiver=System.RECEIVER.format(phone_number=TEST_PHONE_NUMBER))
-    print(f'Message sent')
+    if is_bear_market:
+        matched_scanner_tickers: List[str] = scanner.get_matched_tickers()
+        print(f'matched_scanner_tickers: {matched_scanner_tickers}')
+        alert_data = Templates.MATCHED_TICKERS_SMS.format(username='Nave', filters=scanner.format_filters(),
+                                                          matched_wanted_tickers=matched_scanner_tickers)
+
+        alert.send_sms(body=alert_data, sender=System.SENDER,
+                       receiver=System.RECEIVER.format(phone_number=TEST_PHONE_NUMBER))
+        print(f'Message sent')
+
+
+class ShortTermTechnologyFlow(Flow):
+    def run(self):
+        bear_market_logic = BearMarketLogic()
 
 
 if __name__ == '__main__':
