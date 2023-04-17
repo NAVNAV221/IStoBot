98d60eccb31dc2e8bc5049a53d51b03a95a396c3
NAVNAV221 <navve2008@gmail.com>
2023-04-17 22:54:31 +0300

    Add preperation function to backtest class

--- models/short_term_sma_method.py
+++ models/short_term_sma_method.py
@@ -1,7 +1,10 @@
+import pandas as pd
+import yfinance
 from backtesting import Backtest, Strategy
 from backtesting.lib import crossover
-
 from backtesting.test import SMA, GOOG
+
+COLUMN_AXIS = 1
 
 
 class SmaCross(Strategy):
@@ -20,10 +23,27 @@ class SmaCross(Strategy):
             self.sell()
 
 
-bt = Backtest(GOOG, SmaCross,
-              cash=10000, commission=.002,
-              exclusive_orders=True)
+def prepare_stock_to_backtest_class(ticker: str) -> pd.DataFrame:
+    ticker_yahoo_object = yfinance.Ticker(ticker)
+    ticker_history = ticker_yahoo_object.history(period="5y")
+    prepared_ticker_history = ticker_history.drop(['Dividends', 'Stock Splits'], axis=COLUMN_AXIS)
 
-output = bt.run()
-print(output)
-# bt.plot()
+    return prepared_ticker_history
+
+
+
+
+def main():
+    g = yfinance.Ticker('GOOG')
+    x = prepare_stock_to_backtest_class('GOOG')
+    print(x)
+    bt = Backtest(x, SmaCross,
+                  cash=10000, commission=.002,
+                  exclusive_orders=True)
+    output = bt.run()
+    print(output)
+    # bt.plot()
+
+
+if __name__ == '__main__':
+    main()
