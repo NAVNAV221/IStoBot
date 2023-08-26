3ba6f53ebddb4be02af99f613dee7ca905275f89
NAVNAV221 <navve2008@gmail.com>
2023-08-26 13:30:40 +0300

    Implement correct double top & bottom calculation.

--- bot/flows/eran_flow/flow.py
+++ bot/flows/eran_flow/flow.py
@@ -1,5 +1,9 @@
-from bot.flows.flow import Flow
+from datetime import datetime, timedelta
+
 from bot.filters.screener.platforms.finviz_platform import FinvizScreener
+from bot.flows.flow import Flow
+from core.components.symbol import Symbol
+from core.components.symbol.utils.doubles import get_last_double_buttom, get_last_double_top
 
 
 class EranFlow(Flow):
@@ -36,6 +40,17 @@ class EranFlow(Flow):
         }
     }
 
+    FINVIZ_NORMAL_PRICE_SCREENER_PROPERTIES = {
+        'filter_type': {
+            'screener': {
+                'screener_platform': 'FINVIZ',
+                'filters': {
+                    'Price': 'Over $30'
+                }
+            }
+        }
+    }
+
     def run(self):
         channel_up_screener = set(FinvizScreener(
             input_data=EranFlow.FINVIZ_CHANNEL_UP_SCREENER_PROPERTIES).get_matched_tickers())
@@ -43,10 +58,15 @@ class EranFlow(Flow):
             input_data=EranFlow.FINVIZ_CHANNEL_DOUBLE_TOP_SCREENER_PROPERTIES).get_matched_tickers())
         double_bottom_screener = set(FinvizScreener(
             input_data=EranFlow.FINVIZ_CHANNEL_DOUBLE_BOTTOM_SCREENER_PROPERTIES).get_matched_tickers())
+        normal_price_screener = set(FinvizScreener(
+            input_data=EranFlow.FINVIZ_NORMAL_PRICE_SCREENER_PROPERTIES).get_matched_tickers())
+
+
 
         self.logger.info(f"len(channel_up_screener): {len(channel_up_screener)}")
         self.logger.info(f"len(double_top_screener): {len(double_top_screener)}")
         self.logger.info(f"len(double_bottom_screener): {len(double_bottom_screener)}")
+        self.logger.info(f"len(normal_price_screener): {len(normal_price_screener)}")
 
         channel_up_double_top_stock: set = channel_up_screener.intersection(double_top_screener)
         self.logger.info(f"channel_up_screener intersect double_top_screener length: {channel_up_double_top_stock}")
@@ -55,7 +75,16 @@ class EranFlow(Flow):
             f"channel_up_screener intersect double_bottom_screener length: {channel_up_double_bottom_stock}")
 
         matched_scanner_tickers: set = channel_up_double_top_stock.union(channel_up_double_bottom_stock)
+        # matched_scanner_tickers: set = matched_scanner_tickers.intersection(channel_up_double_bottom_stock)
         self.logger.info(f'matched_scanner_tickers: {matched_scanner_tickers}')
+
+        for ticker in matched_scanner_tickers:
+            s = Symbol(ticker=ticker, end_date=end_date, start_date=start_date)
+        #
+        #     double_buttom_candles = get_last_double_buttom(s)
+        #     double_top_candles = get_last_double_top(s)
+        #
+        #     s.risk_reward_ratio(entry=s.price, stop=double_buttom_candles[0]['Close'], target=double_top_candles[0]['Close'])
 
         # alert = Sms()
         # client = Client()
@@ -91,8 +120,12 @@ class EranFlow(Flow):
         #
         #     except Exception as err:
         #         self.logger.info(f'Cant buy stock according to the following errors: {err}')
-        #
+
+
 
 if __name__ == '__main__':
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=180)
+
     s = EranFlow()
     s.run()
